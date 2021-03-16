cc6c9ccd26c76ea1617f51c64ca2f1435e131b25
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-03-16 15:01:48 +0700

    Create latihanMingguke2_b.java

---
+++ minggu ke 2/Latihan/latihanMingguke2_b.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author MUHAMMAD YUSRIL
+ */
+public class latihanMingguke2_b {
+    public static void main(String[] args) {
+        String nama = "Muhammad Yusril Amin";
+        char Gol = 'B';
+        int b = 12;
+        double c = 12.5;
+        
+        System.out.println (nama);
+        System.out.println(Gol);
+    }
+}
