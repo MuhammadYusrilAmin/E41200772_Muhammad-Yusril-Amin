2c0648eec900a8e90a004a1e339b1e2d4ef6ac82
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-03-16 15:04:14 +0700

    Create tugasMingguke2_c.java

---
+++ minggu ke 2/Tugas/tugasMingguke2_c.java
@@ -0,0 +1,22 @@
+package tugas;
+
+public class tugasMingguke2_c {
+    public static void main(String[] args) {
+        boolean gameOver = false;
+        int students =50, classes=3;
+        double sales_tax;
+        short number1;
+        
+        //penamaan variabel yang salah 
+        int 2beOrNot2Be;//untuk nama variabel tidak boleh di awali dengan angka
+        float price index;//tidak boleh ada spasi apabila ingin memisahkan kata dapat menggunakan _
+        double lastYear'sPrice; // tidak boleh menggunakan tanda petik karena kata atau huruf setelah dalam tanda petik satu akan dideklarasikan menjadi variable char dan tanda petik pembuka harus ditutup dengan tanda petik juga
+        long class; // tidak boleh memberi nama variable dengan nama "class" karena "class" juga merupakan salah satu syntax yang di gunakan pada java sehingga tidak dapat digunakan sebagai nama variable
+   
+        //pembetulan atau penamaan nama variable yang benar
+        int beOrNot2Be;
+        float price_index;
+        double lastYearsPrice;
+        long class1;
+    }
+}
