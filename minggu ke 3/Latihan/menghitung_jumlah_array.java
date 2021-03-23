9ffc1a5c19152e9de6a4386705b8467117770e35
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-03-23 15:35:54 +0700

    Create menghitung_jumlah_array.java

---
+++ minggu ke 3/Latihan/menghitung_jumlah_array.java
@@ -0,0 +1,11 @@
+package latihan;
+
+public class menghitung_jumlah_array {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] array ={3, 7, 8, 1, 10, 20, 35};
+        
+        System.out.println("Menghitung Jumlah Array = " + array.length);
+    }
+    
+}
