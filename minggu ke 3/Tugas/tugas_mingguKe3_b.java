17a28936af8e43a79c0927cd49b3d22ec00cf6dc
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-03-23 15:36:30 +0700

    Create tugas_mingguKe3_b.java

---
+++ minggu ke 3/Tugas/tugas_mingguKe3_b.java
@@ -0,0 +1,12 @@
+package tugas;
+
+public class tugas_mingguKe3_b {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] angka ={7, 34, 23, 1, 26, 19};
+        
+        for (int i =0; i < angka.length; i++){
+            System.out.println(angka[i]);
+        } 
+    }
+}
