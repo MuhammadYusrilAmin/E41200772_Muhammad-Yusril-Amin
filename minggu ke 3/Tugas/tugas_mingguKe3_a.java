d273796d2342d49de3e030a1c6531517434db0ad
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-03-23 15:36:24 +0700

    Create tugas_mingguKe3_a.java

---
+++ minggu ke 3/Tugas/tugas_mingguKe3_a.java
@@ -0,0 +1,13 @@
+package tugas;
+
+public class tugas_mingguKe3_a {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] nama={"Muhammad", "Yusril", "Amin"};
+        
+        for (int i =0; i < nama.length; i++){
+            System.out.print(nama[i]);
+            System.out.print(" ");
+        }
+    }
+}
