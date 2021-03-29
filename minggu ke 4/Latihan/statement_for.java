1324a03869d45c34abd5a2fb9aa98d7a6a222a30
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-03-29 19:30:26 +0700

    Create statement_for.java

---
+++ minggu ke 4/Latihan/statement_for.java
@@ -0,0 +1,12 @@
+package latihan;
+public class statement_for {
+  public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 0;i <= 5; i++){
+            for(int j = 0;j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }    
+}
