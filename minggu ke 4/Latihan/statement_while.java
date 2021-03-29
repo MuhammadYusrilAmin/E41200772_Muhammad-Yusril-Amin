4a8b0594ab951089ebca14d2bc767310a28c97d6
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-03-29 19:31:00 +0700

    Create statement_while.java

---
+++ minggu ke 4/Latihan/statement_while.java
@@ -0,0 +1,13 @@
+package latihan;
+public class statement_while {
+    public static void main(String[] args) {
+        // membuat variable
+        int i = 0;
+        while(i <= 10){
+            // blok kode yang akan di ulang
+            System.out.println("Perulangan ke-" + i);
+            // increment nilai i
+            i++;
+        }
+    }  
+}
