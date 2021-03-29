8fe762d4e2e43b53150427c205f491555c57bc12
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-03-29 19:30:44 +0700

    Create statement_forEach.java

---
+++ minggu ke 4/Latihan/statement_forEach.java
@@ -0,0 +1,12 @@
+package latihan;
+public class statement_forEach {
+    public static void main(String[] args) {
+        // Membuat array
+        int angka[] = {3, 1, 42, 24, 12};
+        
+        // menggunakan perulangan For each untuk perulangan data
+        for(int x : angka){
+            System.out.print(x + " ");
+        }
+    }  
+}
