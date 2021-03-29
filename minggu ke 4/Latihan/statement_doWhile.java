2c8c7f6af36d404aa673dd3d5f55927b1fd7030c
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-03-29 19:30:11 +0700

    Create statement_doWhile.java

---
+++ minggu ke 4/Latihan/statement_doWhile.java
@@ -0,0 +1,11 @@
+package latihan;
+public class statement_doWhile {
+    public static void main(String[] args) {
+        // Membuat variable
+        int i = 0;
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }while(i <= 10);
+    } 
+}
