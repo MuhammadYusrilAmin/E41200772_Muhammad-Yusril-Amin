a055c39e4b80f35cc7fe58dff74ac965a6ebcc70
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-04-03 12:58:52 +0700

    Update tugas_mingguKe4_b.java

--- minggu ke 4/Tugas/tugas_mingguKe4_b.java
+++ minggu ke 4/Tugas/tugas_mingguKe4_b.java
@@ -8,13 +8,29 @@ public class tugas_mingguKe4_b {
         System.out.println("============================");
         
         // Membuat variable 
-        int angka = 1;
-        
+        int angka = 2;
+        System.out.print("1");
         do {
-            System.out.print("  "+angka);
+            System.out.print(" "+angka);
             // increment nilai angka
-            angka *= 2;
+            angka += 2;
         }while(angka <= 100);
         System.out.println("");
-    } 
+        System.out.println("");
+        
+        // header2
+        System.out.println("         Do_While ");
+        System.out.println("Bilangan perkalian 2 (1-100)");
+        System.out.println("============================");
+        
+        // Membuat variable 
+        int angka1 = 1;
+        
+        do {
+            System.out.print("  "+angka1);
+            // increment nilai angka
+            angka1 *= 2;
+        }while(angka1 <= 100);
+        System.out.println("");
+    }
 }
