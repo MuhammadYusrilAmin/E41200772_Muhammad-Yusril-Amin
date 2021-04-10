55c66684076ec6aab4567b621647015d7143d596
Muhammad Yusril Amin <muhammadyusril28*@users.noreply.github.com>
2021-04-10 22:57:12 +0700

    Create Node.java

---
+++ minggu ke 5/Binary tree/Node.java
@@ -0,0 +1,13 @@
+package wdsi_mingguke5;
+
+public class Node {
+    public int id;
+    public String data;
+    public Node leftChild;
+    public Node rightChild;
+    
+    public void displayNode(){
+        System.out.println("{ "+ id +", "+ data +" }");
+    }
+    
+}
